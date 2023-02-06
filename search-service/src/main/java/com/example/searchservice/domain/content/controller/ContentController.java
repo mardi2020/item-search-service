6e44a8fde4bb0d42e67e46dfb4d4acf77a63d848
mardi2020 <mardi@khu.ac.kr>
2023-02-06 18:34:35 +0900

    fix: api end point 수정

--- search-service/src/main/java/com/example/searchservice/domain/content/controller/ContentController.java
+++ search-service/src/main/java/com/example/searchservice/domain/content/controller/ContentController.java
@@ -1,7 +1,6 @@
 package com.example.searchservice.domain.content.controller;
 
 
-import com.example.searchservice.domain.content.ContentIndex;
 import com.example.searchservice.domain.content.dto.response.ContentResponse;
 import com.example.searchservice.domain.content.service.ContentService;
 import java.util.List;
@@ -13,12 +12,14 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.data.web.SortDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 @Slf4j
+@RequestMapping("/contents")
 public class ContentController {
 
     private final ContentService contentService;
