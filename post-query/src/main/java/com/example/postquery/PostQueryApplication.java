a2dbfee027c5753a30cfc0cb61c4280973234c9e
mardi2020 <mardi@khu.ac.kr>
2023-02-07 21:06:45 +0900

    chore: content-query 모듈 추가

---
+++ post-query/src/main/java/com/example/postquery/PostQueryApplication.java
@@ -0,0 +1,13 @@
+package com.example.postquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostQueryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostQueryApplication.class, args);
+    }
+
+}
