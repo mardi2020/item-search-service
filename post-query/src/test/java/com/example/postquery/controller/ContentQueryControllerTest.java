8af4e1211274cff0b63853c8ebe60e967bec5696
mardi2020 <mardi@khu.ac.kr>
2023-02-08 17:23:24 +0900

    test: 해시태그, 북마크, 게시글 좋아요 api 호출 테스트

--- post-query/src/test/java/com/example/postquery/controller/ContentQueryControllerTest.java
+++ post-query/src/test/java/com/example/postquery/controller/ContentQueryControllerTest.java
@@ -2,15 +2,22 @@ package com.example.postquery.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.example.postquery.entity.Bookmark;
 import com.example.postquery.entity.Content;
+import com.example.postquery.entity.ContentLike;
+import com.example.postquery.entity.Hashtag;
 import com.example.postquery.entity.ImageUrl;
+import com.example.postquery.repository.BookmarkQueryRepository;
+import com.example.postquery.repository.ContentLikeQueryRepository;
 import com.example.postquery.repository.ContentQueryRepository;
+import com.example.postquery.repository.HashtagQueryRepository;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import java.util.List;
 import java.util.Set;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -36,6 +43,15 @@ class ContentQueryControllerTest {
     @Autowired
     private ContentQueryRepository contentQueryRepository;
 
+    @Autowired
+    private HashtagQueryRepository hashtagQueryRepository;
+
+    @Autowired
+    private BookmarkQueryRepository bookmarkQueryRepository;
+
+    @Autowired
+    private ContentLikeQueryRepository contentLikeQueryRepository;
+
     @BeforeEach
     void setUp() {
         Content content = Content.builder()
@@ -49,6 +65,14 @@ class ContentQueryControllerTest {
                 .modifiedAt("2023-01-01")
                 .build();
         contentQueryRepository.save(content);
+    }
+
+    @AfterEach
+    void tearDown() {
+        contentQueryRepository.deleteAll();
+        hashtagQueryRepository.deleteAll();
+        bookmarkQueryRepository.deleteAll();
+        contentLikeQueryRepository.deleteAll();
     }
 
     @DisplayName("게시글 id로 세부 조회할 수 있다")
@@ -111,6 +135,82 @@ class ContentQueryControllerTest {
                 .ignoringActualNullFields()
                 .isEqualTo(expected);
     }
+
+    @DisplayName("해시태그 이름으로 id를 얻는다")
+    @Test
+    void findByHashtagId() {
+        // given
+        final String name = "아이유";
+        final Long expected = 1L;
+        String api = "http://localhost:" + port + "/hashtags?h=" + name;
+        hashtagQueryRepository.save(new Hashtag(expected, name));
+
+        // when
+        ExtractableResponse<Response> response = get(api);
+        Long actual = response.body().as(Long.class);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("사용자의 북마크 리스트를 조회한다")
+    @Test
+    void findBookmarkByUserId() {
+        // given
+        final Long userId = 1L;
+        String api = "http://localhost:" + port + "/bookmarks/" + userId;
+        Bookmark bookmark1 = Bookmark.builder()
+                .bookmarkId(1L)
+                .userId(userId)
+                .contentId(1L)
+                .commentCount(5)
+                .likesCount(222)
+                .build();
+        Bookmark bookmark2 = Bookmark.builder()
+                .bookmarkId(2L)
+                .userId(userId)
+                .contentId(2L)
+                .commentCount(9)
+                .likesCount(44)
+                .build();
+
+        Bookmark bookmark3 = Bookmark.builder()
+                .bookmarkId(3L)
+                .userId(userId)
+                .contentId(3L)
+                .commentCount(11111)
+                .likesCount(1613)
+                .build();
+        bookmarkQueryRepository.saveAll(List.of(bookmark1, bookmark2, bookmark3));
+
+        // when
+        ExtractableResponse<Response> response = get(api);
+        Bookmark[] result = response.body().as(Bookmark[].class);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(result.length).isEqualTo(3);
+    }
+
+    @DisplayName("사용자가 게시글을 좋아요했는지 확인한다")
+    @Test
+    void isLikeContent() {
+        // given
+        final Long userId = 1L;
+        final Long contentId = 1L;
+        String api = "http://localhost:" + port + "/likes/" + contentId + "/" + userId;
+        contentLikeQueryRepository.save(new ContentLike(1L, userId, contentId));
+
+        // when
+        ExtractableResponse<Response> response = get(api);
+        Boolean result = response.body().as(Boolean.class);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(result).isEqualTo(true);
+    }
+
     static ExtractableResponse<Response> get(final String api) {
         return RestAssured
                 .given().log().all()
