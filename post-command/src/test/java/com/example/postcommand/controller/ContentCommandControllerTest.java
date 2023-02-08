605d65719e6cf95e9a6ed54712a3cd9023510523
mardi2020 <mardi@khu.ac.kr>
2023-02-08 14:46:53 +0900

    feat: content 컨트롤러, 서비스 로직 테스트

---
+++ post-command/src/test/java/com/example/postcommand/controller/ContentCommandControllerTest.java
@@ -0,0 +1,196 @@
+package com.example.postcommand.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.postcommand.dto.request.ContentCreateRequest;
+import com.example.postcommand.dto.request.ContentUpdateRequest;
+import com.example.postcommand.entity.ImageUrl;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+class ContentCommandControllerTest {
+
+    @LocalServerPort
+    int port;
+
+    @Test
+    @DisplayName("content를 저장한다")
+    void save() {
+        // given
+        Long userId = 1L;
+        List<ImageUrl> imageUrl = List.of(new ImageUrl("https://mockup.url", 1, userId));
+        ContentCreateRequest contentCreateRequest = ContentCreateRequest.builder()
+                .userId(userId)
+                .visibleComments(true)
+                .visibleLikes(true)
+                .imageUrl(imageUrl)
+                .text("변경전 텍스트 #aaa #bb #ccc #dde")
+                .hashtags(Set.of(1L, 2L, 3L, 4L))
+                .build();
+
+        // when
+        ExtractableResponse<Response> response = saveContent("http://localhost:"+ port + "/contents", contentCreateRequest);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    @Test
+    @DisplayName("content를 수정한다")
+    void update() {
+        // given
+        final long contentId = 1L;
+        final Long userId = 1L;
+
+        ContentCreateRequest createRequest = ContentCreateRequest.builder()
+                .userId(userId)
+                .visibleComments(true)
+                .visibleLikes(true)
+                .imageUrl(List.of(new ImageUrl("https://before.url", 1, userId)))
+                .text("변경전 텍스트 #aaa #bb #ccc #dde")
+                .hashtags(Set.of(1L, 2L, 3L, 4L))
+                .build();
+
+        ContentUpdateRequest updateRequest = ContentUpdateRequest.builder()
+                .text("변경된 텍스트 #aaa")
+                .hashtags(Set.of(1L))
+                .visibleLikes(false)
+                .visibleComments(false)
+                .imageUrl(List.of(new ImageUrl("https://after.url", 1, userId)))
+                .userId(userId)
+                .build();
+        // when
+        final String api = "http://localhost:" + port + "/contents";
+        saveContent(api, createRequest);
+        ExtractableResponse<Response> response = updateContent(api + "/" + contentId, updateRequest);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    @Test
+    @DisplayName("content를 삭제한다")
+    void delete() {
+        // given
+        final long contentId = 1L;
+        final Long userId = 1L;
+
+        ContentCreateRequest createRequest = ContentCreateRequest.builder()
+                .userId(userId)
+                .visibleComments(true)
+                .visibleLikes(true)
+                .imageUrl(List.of(new ImageUrl("https://before.url", 1, userId)))
+                .text("description")
+                .hashtags(new HashSet<>())
+                .build();
+
+        // when
+        final String api = "http://localhost:" + port + "/contents";
+        saveContent(api, createRequest);
+        ExtractableResponse<Response> response = deleteContent(api + "/" + contentId);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+    }
+
+    @Test
+    @DisplayName("좋아요 수를 가린다")
+    void hide_likes() {
+        // given
+        final long contentId = 1;
+        ContentCreateRequest createRequest = ContentCreateRequest.builder()
+                .userId(1L)
+                .visibleComments(true)
+                .visibleLikes(true)
+                .imageUrl(List.of(new ImageUrl("https://before.url", 1, 1L)))
+                .text("description")
+                .hashtags(new HashSet<>())
+                .build();
+        final String api = "http://localhost:" + port + "/contents";
+        saveContent(api, createRequest);
+
+        // when
+        ExtractableResponse<Response> response = saveVisibility(api + "/visibility/likes/" + contentId);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    @Test
+    @DisplayName("댓글을 숨긴다")
+    void hide_comments() {
+        // given
+        final long contentId = 1;
+        ContentCreateRequest createRequest = ContentCreateRequest.builder()
+                .userId(1L)
+                .visibleComments(true)
+                .visibleLikes(true)
+                .imageUrl(List.of(new ImageUrl("https://before.url", 1, 1L)))
+                .text("description")
+                .hashtags(new HashSet<>())
+                .build();
+        final String api = "http://localhost:" + port + "/contents";
+        saveContent(api, createRequest);
+
+        // when
+        ExtractableResponse<Response> response = saveVisibility(api + "/visibility/comments/" + contentId);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    static ExtractableResponse<Response> saveContent(final String api, final ContentCreateRequest request) {
+        return RestAssured
+                .given().log().all()
+                .accept("application/json")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when().post(api)
+                .then().log().all()
+                .extract();
+    }
+
+    static ExtractableResponse<Response> saveVisibility(final String api) {
+        return RestAssured
+                .given().log().all()
+                .when().post(api)
+                .then().log().all()
+                .extract();
+    }
+
+    static ExtractableResponse<Response> updateContent(final String api, final ContentUpdateRequest request) {
+        return RestAssured
+                .given().log().all()
+                .accept("application/json")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when().patch(api)
+                .then().log().all()
+                .extract();
+    }
+
+    static ExtractableResponse<Response> deleteContent(final String api) {
+        return RestAssured
+                .given().log().all()
+                .when().delete(api)
+                .then().log().all()
+                .extract();
+    }
+}
---
+++ post-command/src/test/java/com/example/postcommand/service/ContentCommandServiceTest.java
@@ -0,0 +1,163 @@
+package com.example.postcommand.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+
+import com.example.postcommand.dto.request.ContentCreateRequest;
+import com.example.postcommand.dto.request.ContentUpdateRequest;
+import com.example.postcommand.entity.Content;
+import com.example.postcommand.entity.ImageUrl;
+import com.example.postcommand.repository.ContentCommandRepository;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ContentCommandServiceTest {
+
+    @Autowired
+    private ContentCommandService contentCommandService;
+    @Autowired
+    private ContentCommandRepository contentCommandRepository;
+
+
+    @DisplayName("새로운 게시글을 생성한다")
+    @Test
+    void 새로운_게시글을_생성한다() {
+        // given
+        Long userId = 1L;
+        String text = "본문 #해시태그 @test1234";
+        boolean visibleComments = false;
+        boolean visibleLikes = false;
+        String url = "http://test.jikji/asd12sdf452";
+        int order = 1;
+        List<ImageUrl> imageUrl = List.of(new ImageUrl(url, order, userId));
+        Set<Long> hashtags = Set.of(1L, 2L, 3L, 4L);
+
+        ContentCreateRequest contentCreateRequest = new ContentCreateRequest(
+                userId, text,
+                visibleComments,
+                visibleLikes,
+                imageUrl,
+                hashtags);
+
+        // when
+        Long savedId = contentCommandService.save(contentCreateRequest);
+
+        // then
+        assertThat(savedId).isNotNull();
+    }
+
+    @DisplayName("게시글을 수정한다")
+    @Test
+    void 게시글을_수정한다() {
+        // given
+        final Long contentId = 1L;
+        Content content = Content.builder()
+                .id(contentId)
+                .likes(0)
+                .text("변경전 텍스트 #aaa #bb #ccc #dde")
+                .imageUrl(List.of(new ImageUrl("http://test.jikji/before-image", 1, 1L)))
+                .userId(1L)
+                .visibleComments(false)
+                .visibleLikes(false)
+                .hashtags(Set.of(1L, 2L, 3L, 4L))
+                .build();
+        contentCommandRepository.save(content);
+
+        ContentUpdateRequest request = ContentUpdateRequest.builder()
+                .text("변경된 텍스트 #aaa")
+                .userId(1L)
+                .imageUrl(List.of(new ImageUrl("http://test.jikji/changed-image-url", 1, 1L)))
+                .visibleComments(true)
+                .visibleLikes(true)
+                .hashtags(Set.of(1L))
+                .build();
+        // when
+        contentCommandService.update(contentId, request);
+        content = contentCommandRepository.findById(contentId).orElseThrow();
+
+        // then
+        assertThat(content.getText()).isEqualTo(request.getText());
+        assertThat(content.getImageUrl()).isEqualTo(request.getImageUrl());
+        assertThat(content.getVisibleComments()).isEqualTo(request.getVisibleComments());
+        assertThat(content.getVisibleLikes()).isEqualTo(request.getVisibleLikes());
+        assertThat(content.getHashtags()).isEqualTo(request.getHashtags());
+    }
+
+    @DisplayName("게시글을 삭제한다")
+    @Test
+    void 게시글을_삭제한다() {
+        // given
+        Long contentId = 1L;
+        Content content = Content.builder()
+                .id(contentId)
+                .likes(0)
+                .text("게시글 본문")
+                .imageUrl(List.of(new ImageUrl("http://test.jikji/image", 1, 1L)))
+                .userId(1L)
+                .visibleComments(false)
+                .visibleLikes(false)
+                .hashtags(new HashSet<>())
+                .build();
+        contentCommandRepository.save(content);
+
+        // when, then
+        assertDoesNotThrow(() -> contentCommandService.delete(contentId));
+    }
+
+    @DisplayName("댓글 감추기 요청시 댓글을 비활성화 한다")
+    @Test
+    void 댓글을_비활성화한다() {
+        // given
+        Long contentId = 1L;
+        boolean visibleComments = true;
+        Content content = Content.builder()
+                .id(contentId)
+                .likes(0)
+                .text("게시글 본문")
+                .imageUrl(List.of(new ImageUrl("http://test.jikji/image", 1, 1L)))
+                .userId(1L)
+                .visibleComments(visibleComments)
+                .hashtags(new HashSet<>())
+                .build();
+        contentCommandRepository.save(content);
+
+        // when
+        boolean result = contentCommandService.visibilityComments(contentId);
+
+        // then
+        assertThat(result).isEqualTo(!visibleComments);
+    }
+
+    @DisplayName("좋아요 숫자 감추기 요청시 좋아요 수를 가린다")
+    @Test
+    void 좋아요수를_가린다() {
+        // given
+        Long contentId = 1L;
+        boolean visibleLikes = true;
+        Content content = Content.builder()
+                .id(contentId)
+                .likes(0)
+                .text("게시글 본문")
+                .imageUrl(List.of(new ImageUrl("http://test.jikji/image", 1, 1L)))
+                .userId(1L)
+                .visibleLikes(visibleLikes)
+                .hashtags(new HashSet<>())
+                .build();
+        contentCommandRepository.save(content);
+
+        // when
+        boolean result = contentCommandService.visibilityLikes(contentId);
+
+        // then
+        assertThat(result).isEqualTo(!visibleLikes);
+    }
+}
