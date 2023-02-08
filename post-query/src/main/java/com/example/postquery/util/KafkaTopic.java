5388461b7e39c51baf73506041ac25250d9f6386
mardi2020 <mardi@khu.ac.kr>
2023-02-08 15:48:19 +0900

    fix: 새로운 해시태그 추가를 위한 kafka topic 추가

---
+++ post-query/src/main/java/com/example/postquery/util/KafkaTopic.java
@@ -0,0 +1,20 @@
+package com.example.postquery.util;
+
+public final class KafkaTopic {
+
+    public static final String ADD_CONTENT = "content-add";
+    public static final String DELETE_CONTENT = "content-delete";
+    public static final String UPDATE_CONTENT = "content-update";
+
+    public static final String ADD_BOOKMARK = "bookmark-add";
+    public static final String DELETE_BOOKMARK = "bookmark-delete";
+
+    public static final String VISIBLE_COMMENT = "comment-visible";
+    public static final String VISIBLE_LIKES = "likes-visible";
+
+    public static final String ADD_CONTENT_LIKE = "like-add";
+    public static final String DELETE_CONTENT_LIKE = "like-delete";
+
+    public static final String UPDATE_HASHTAG = "hashtag-update";
+    public static final String NEW_HASHTAG = "hashtag-new";
+}
