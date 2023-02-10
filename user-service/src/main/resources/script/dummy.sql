63f93287dad8ebe853c897390d57626fb808e3bc
mardi2020 <mardi@khu.ac.kr>
2023-02-10 15:17:55 +0900

    chore: 더미 테이터 추가

---
+++ user-service/src/main/resources/script/dummy.sql
@@ -0,0 +1,23 @@
+insert into join_users(created_at, login_id, name) values('2023-01-02 01:10', 'qewrty1234', '아이유 이지금');
+insert into join_users(created_at, login_id, name) values('2023-01-02 02:00', 'dlwlrma', '아이유 dlwlrma');
+insert into join_users(created_at, login_id, name) values('2023-01-02 03:00', 'asd123123', '아이유 이지금');
+insert into join_users(created_at, login_id, name) values('2023-01-02 04:00', 'georginaamoros', 'Georgina Amoros');
+insert into join_users(created_at, login_id, name) values('2023-01-02 05:00', 'newjeans_official', 'NewJeans');
+
+insert into join_users(created_at, login_id, name) values('2023-01-02 06:00', 'le_sserafim', 'LE SSERAFIM');
+insert into join_users(created_at, login_id, name) values('2023-01-02 06:00', 'hhh.e_c.v', 'HONG EUNCHAE of LE SSERAFIM');
+insert into join_users(created_at, login_id, name) values('2023-01-02 07:00', 'k_a_z_u_h_a__', 'KAZUHA of LE SSERAFIM');
+insert into join_users(created_at, login_id, name) values('2023-01-02 08:10', 'stayc_highup', 'STAYC(스테이씨)');
+insert into join_users(created_at, login_id, name) values('2023-01-02 08:10', 'arianagrande', 'Ariana Grande');
+
+insert into join_users(created_at, login_id, name) values('2023-01-03 09:00', 'faker', 'Faker(페이커)');
+insert into join_users(created_at, login_id, name) values('2023-01-04 09:00', 'quanhaha79', 'ha dong hoon');
+insert into join_users(created_at, login_id, name) values('2023-01-05 09:00', 'netflixkr', 'Netflix Korea');
+insert into join_users(created_at, login_id, name) values('2023-01-06 09:00', 'skuukzky', '숮이 💄💅👡👠🎀👙🌂👗🌂🎀💋💌');
+insert into join_users(created_at, login_id, name) values('2023-01-07 09:00', 'sbs_runningman_sbs', '런닝맨 RunningMan');
+
+insert into join_users(created_at, login_id, name) values('2023-01-10 09:00', 'jsomin86', '쏘미닝');
+insert into join_users(created_at, login_id, name) values('2023-01-11 09:00', 'rkive', 'RM');
+insert into join_users(created_at, login_id, name) values('2023-01-12 09:00', 'bts.bighitofficial', 'BTS official');
+insert into join_users(created_at, login_id, name) values('2023-01-12 09:00', 'aespa_official', 'aespa 에스파');
+insert into join_users(created_at, login_id, name) values('2023-01-13 09:00', 'taylorswift', 'Taylor Swift');
